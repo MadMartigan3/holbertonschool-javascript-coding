47122f78654ffcdc21c2b5f5b1fa69bd2c66466c
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-05-29 16:25:17 +0200

    corection task 2/6

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,39 +2,23 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    
-    const lines = data.split('\n');
-    const students = lines.filter(line => line.trim() !== '');
-    
-    if (students.length === 0) {
-      console.log('Nombre d\'étudiants : 0');
-      return;
-    }
+    const data = fs.readFileSync(path, 'utf-8');
 
-    const header = students.shift();
-    
-    const studentCount = students.length;
-    console.log(`Nombre d'étudiants : ${studentCount}`);
-    
-    const fields = {};
-
-    students.forEach(student => {
-      const [firstName, lastName, age, field] = student.split(',');
-      
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      
-      fields[field].push(firstName);
+    const students = data.split('\n').filter(Boolean).slice(1).map((line) => {
+      const [firstName, lastName, age, field] = line.split(',');
+      return {
+        firstName, lastName, age, field,
+      };
     });
 
-    for (const [field, names] of Object.entries(fields)) {
-      console.log(`Nombre d'étudiants en ${field} : ${names.length}. Liste : ${names.join(', ')}`);
-    }
+    const csStudents = students.filter((student) => student.field === 'CS');
+    const sweStudents = students.filter((student) => student.field === 'SWE');
 
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student.firstName).join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student.firstName).join(', ')}`);
   } catch (error) {
-    throw new Error('Impossible de charger la base de données');
+    throw new Error('Cannot load the database');
   }
 }
 
